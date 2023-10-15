f0df9fabb987a55962718a0a59dd289e580dd832
threeWindows <thec76ure@gmail.com>
2023-10-15 15:53:50 +0200

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ import GanreList from "./components/GanreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGanres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <div>
@@ -33,8 +38,14 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area={`main`}>
-          <PlatformSelector />
-          <GameGrid selectedGenre={selectedGenre} />
+          <PlatformSelector
+            selectedPlatform={selectedPlatform}
+            onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+          />
+          <GameGrid
+            selectedPlatform={selectedPlatform}
+            selectedGenre={selectedGenre}
+          />
         </GridItem>
       </Grid>
     </div>
